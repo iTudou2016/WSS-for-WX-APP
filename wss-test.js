6d4fe3836a979b46a28e25d7174200c5eeaaa8d4
iTudou2016 <34324426+iTudou2016@users.noreply.github.com>
2017-12-13 16:04:47 +0800

    Update wss-test.js

--- wss-test.js
+++ wss-test.js
@@ -5,29 +5,31 @@
 const https = require('https');
 const fs = require('fs');
 const WebSocketServer = require('ws').Server;
+
 //定义安全证书
 const options = {
   "key": fs.readFileSync("ssl.key"),
   "cert": fs.readFileSync("ssl.crt")
 };
+
 //创建https服务器对象
 const httpsServer = https.createServer(options, function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+    res.setHead(200, {'Content-Type', 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080, () => {
     console.log('[' + new Date() + '] Serveris listening on port 8080')
 })
-var wss = new WebSocketServer({
+
+var wsss = new WebSocketServer({
     server: httpsServer
 });
-wss.on('connection', function(wsConnect) {
-    wsConnect.on('message', function(message) {
+wsss.on('connection', function(wssConnect) {
+    wssConnect.on('message', function(message) {
         console.log(message);
         if (message.type === 'utf8') {
             console.log('>> message content from client: ' + message.utf8Data)
             connection.sendUTF('[from server] ' + message.utf8Data)
         }
-        wsConnect.send('reply');
+        wssConnect.send('reply');
     });
 });
