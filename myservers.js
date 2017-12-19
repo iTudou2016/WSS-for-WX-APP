64ca57266ef1fef78fc2643c6086f189a47ad837
iTudou2016 <34324426+iTudou2016@users.noreply.github.com>
2017-12-19 23:33:57 +0800

    Update myservers.js

--- myservers.js
+++ myservers.js
@@ -5,12 +5,13 @@ const https = require('https');
 //const express = require('express');
 const fs = require('fs');
 const wsServer = require('ws').Server;
+const WebSocket = require('ws');
 const wtproxy = require('./webtelnet-proxy.js');
 
-//const options = {
-//  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-//  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
-//};
+const options = {
+  "key": fs.readFileSync('ssl.key'),
+  "cert": fs.readFileSync('ssl.crt')
+};
 
 const telnetconf = {
     host: '127.0.0.1',
@@ -24,20 +25,20 @@ const wsconf = {
     host: '0.0.0.0',
     port: 7777,	
 };
-
-//web server
-var webSvr = http.createServer(function(req, res) {
+/*
+//https server
+var httpsSvr = https.createServer(options, function(req, res) {
     res.writeHead(200, {'Content-Type':'text/plain'});
-    res.write('welcome to 91Mud, web content will show here later');
-    res.end('I am web server');
+    res.write('welcome to 91Mud, web content will show here later\n');
+    res.end('I am web server\n');
 });
 
-webSvr.listen(webconf.port, function() {
+httpsSvr.listen(webconf.port, function() {
     console.log('web server running at port ' + webconf.port)
 });
 
 //websocket server
-var wsSvr = http.createServer(function(req, res) {
+var wsSvr = https.createServer(options, function(req, res) {
     res.writeHead(200, {'Content-Type':'text/plain'});
     res.end('I am websocket server');
 });
@@ -49,15 +50,20 @@ wsSvr.listen(wsconf.port, function() {
 var wss = new wsServer({
     server: wsSvr
 });
-
+*/
+var wss = new WebSocket.Server({port: 8080});
+/*
 wss.on('connection', function(wsConnect) {
 
     wsConnect.on('message', function(message) {
         console.log(message);
         wsConnect.send('reply: ' + message);
-    });
+     });
 });
-
+*/
 // create webtelnet proxy and bind to wss
 var mudproxy = wtproxy(wss, telnetconf.port, telnetconf.host);
 mudproxy.setCharset('gbk');
+// create webtelnet proxy and bind to wss
+//var mudproxy = wtproxy(wss, telnetconf.port, telnetconf.host);
+//mudproxy.setCharset('gbk');
