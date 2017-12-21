252fab5aebc835ca5e0b27d33b4839d29f1d3b6b
iTudou2016 <34324426+iTudou2016@users.noreply.github.com>
2017-12-21 23:15:02 +0800

    Update wxclient.js

--- wxapp/wxclient.js
+++ wxapp/wxclient.js
@@ -1,13 +1,16 @@
 // pages/wsclient/wsclient.js
 
 //var writeServerData = require('writeServerData.js')
-var ansi_up = require('ansi_up.js')
+var ansi_up = require('ansi_up.js');
+var WxParse = require('../../wxParse/wxParse.js'); 
 
 Page({
   
   data: {
     outString: "This is page data.",
+    inputValue: "",
     inputFocus: true,    
+    scrolltop: 10
 
   },
 
@@ -82,6 +85,21 @@ binayUtf8ToString: function (buf, begin) {
   },
 
 writeToScreen: function (str) {
+    
+    
+    var article = '<div>我是HTML代码</div>';
+    /**
+    * WxParse.wxParse(bindName , type, data, target,imagePadding)
+    * 1.bindName绑定的数据名(必填)
+    * 2.type可以为html或者md(必填)
+    * 3.data为传入的具体数据(必填)
+    * 4.target为Page对象,一般为this(必填)
+    * 5.imagePadding为当图片自适应是左右的单一padding(默认为0,可选)
+    */
+    var that = this;
+    WxParse.wxParse('article', 'html', str, that, 5);
+
+    
     this.setData({
       outString: this.data.outString + str
     });
@@ -161,7 +179,8 @@ writeServerData: function (buf) {
       data: e.detail.value + "\n",
     });
     this.setData({
-      inputFocus: true
+      inputValue: "",
+      inputFocus: true,      
     })
   },
 
