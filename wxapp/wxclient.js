3dc185763cd745bb00ecaa35b87e3e3d1aba8f1b
iTudou2016 <34324426+iTudou2016@users.noreply.github.com>
2017-12-22 09:07:06 +0800

    Update wxclient.js

--- wxapp/wxclient.js
+++ wxapp/wxclient.js
@@ -1,8 +1,6 @@
 // pages/wsclient/wsclient.js
 
-//var writeServerData = require('writeServerData.js')
 var ansi_up = require('ansi_up.js');
-var WxParse = require('../../wxParse/wxParse.js'); 
 
 Page({
   
@@ -85,21 +83,7 @@ binayUtf8ToString: function (buf, begin) {
   },
 
 writeToScreen: function (str) {
-    
-    
-    var article = '<div>我是HTML代码</div>';
-    /**
-    * WxParse.wxParse(bindName , type, data, target,imagePadding)
-    * 1.bindName绑定的数据名(必填)
-    * 2.type可以为html或者md(必填)
-    * 3.data为传入的具体数据(必填)
-    * 4.target为Page对象,一般为this(必填)
-    * 5.imagePadding为当图片自适应是左右的单一padding(默认为0,可选)
-    */
-    var that = this;
-    WxParse.wxParse('article', 'html', str, that, 5);
-
-    
+  
     this.setData({
       outString: this.data.outString + str
     });
